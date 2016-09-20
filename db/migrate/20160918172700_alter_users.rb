0d558c439709383ffefa7a61cb222d367348e816
kinseyost <kinsey.vanost@gmail.com>
2016-09-20 03:19:55 +0000

    Fixing broken migration 2/3

--- db/migrate/20160918172700_alter_users.rb
+++ db/migrate/20160918172700_alter_users.rb
@@ -4,7 +4,7 @@ class AlterUsers < ActiveRecord::Migration[5.0]
     rename_table("users", "admin_users")
     add_column("admin_users", "username", :string, :limit => 25, :after => "email")
     change_column("admin_users", "email", :string, :limit => 100)
-    rename_column("admin_users", "breakit", "hashed_password")
+    rename_column("admin_users", "password", "hashed_password")
     puts("Always add an index for foreign keys and columns that are used frequently!")
     add_index("admin_users", "username")
   end
