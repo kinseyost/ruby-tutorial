54f2d96555d3e355ec9b429d46ab36ac66c57489
kinseyost <kinsey.vanost@gmail.com>
2016-09-21 01:00:04 +0000

    Changes names based on AlterUsers migration

---
+++ app/models/admin_user.rb
@@ -0,0 +1,7 @@
+class AdminUser < ApplicationRecord
+    
+    # To configure a different table name.
+    # self.table_name = "admin_users"
+    
+    # ApplicationRecord already knows about the table because it is inheriting from ActiveRecord::Base
+end
