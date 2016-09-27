5e8c6672345a26cb371387ea10ef7e771900300b
kinseyost <kinsey.vanost@gmail.com>
2016-09-26 21:54:25 -0700

    Using Many-to-many associations

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,7 +1,9 @@
 class AdminUser < ApplicationRecord
-    
+
+  has_and_belongs_to_many :pages
+
     # To configure a different table name.
     # self.table_name = "admin_users"
-    
+
     # ApplicationRecord already knows about the table because it is inheriting from ActiveRecord::Base
 end
--- app/models/page.rb
+++ app/models/page.rb
@@ -1,5 +1,9 @@
 class Page < ApplicationRecord
 
   belongs_to :subject
+  # specify the correct table using class_name (with capital case)
+  has_and_belongs_to_many :editors, :class_name => "AdminUser"
+  # If join table has different name, you specify (in both sides of the relationship) like this:
+  #  has_and_belongs_to_many :admin_users, :join_table => "admin_users_pages"
 
 end
