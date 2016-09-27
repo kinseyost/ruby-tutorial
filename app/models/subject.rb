6973e43a1f2fbbc50ac05ab5c81755b01cf142ce
kinseyost <kinsey.vanost@gmail.com>
2016-09-26 20:32:20 -0700

    Adding and using named scopes to models

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,2 +1,13 @@
 class Subject < ApplicationRecord
+
+  # Lamda is read at execution time, which is important for dates.
+  scope :visible, lambda { where(:visible => true) }
+  # Stabby lambda syntax is subtly different, but can also be used.
+  scope :invisible, -> { where(:visible => false) }
+  scope :sorted, lambda { order("subjects.position ASC") }
+  scope :newest_first, lambda { order("subjects.created_at DESC") }
+  scope :search, lambda { |query|
+    where(["name LIKE ?", "%${query}%"])
+  }
+
 end
